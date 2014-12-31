7a884ee5fb4ec9bc22ad94af2eb9469560e109af
Steve Westwood <steve.westwood@hexsaw.org.uk>
2014-12-31 15:32:12 +0000

    Remove Heroku settings

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: java  $JAVA_OPTS -jar target/logactaesque-game-0.0.1-SNAPSHOT.jar 
--- src/main/java/uk/org/hexsaw/logactaesque/game/Application.java
+++ src/main/java/uk/org/hexsaw/logactaesque/game/Application.java
@@ -11,13 +11,6 @@ import org.springframework.context.annotation.Configuration;
 public class Application {
 
     public static void main(String[] args) {
-        // Heroku requirement
-        // See http://nicholaspaulsmith.com/spring-boot-on-heroku/
-        String webPort = System.getenv("PORT");
-        if (webPort == null || webPort.isEmpty()) {
-            webPort = "8080";
-        }
-        System.setProperty("server.port", webPort);
         SpringApplication.run(Application.class, args);
     }
 }
